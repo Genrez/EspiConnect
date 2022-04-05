623a036742a6f4e2b2df4f6eec59b750f0ffa506
GenRex <richardngo2000@gmail.com>
2022-04-06 00:48:09 +1000

    First character animated

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
+    public Animator animator;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+       
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+
+
+
+        transform.position = transform.position + movement * speed;
     }
 
 
