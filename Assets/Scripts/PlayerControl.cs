d40d13aad6f2ab67c3464ad65d1109dbf14f2a0a
GenRex <richardngo2000@gmail.com>
2022-04-05 20:13:26 +1000

    Update to 2021.2.18f1

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal;
+    }
+
+
+}
