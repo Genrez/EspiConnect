3214f38c6434b0098e482457ab22ec3095fc4b68
GenRex <richardngo2000@gmail.com>
2022-04-06 16:10:16 +1000

    Added Indictator

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float interpSpeed;
     public Animator animator;
     public float speed;
+    private Vector3 targetPos;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-       
+
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Magnitude", movement.magnitude);
-
-
-
-
         transform.position = transform.position + movement * speed;
-    }
 
+        if (GetComponent<Rigidbody2D>().velocity.magnitude >= 0.001)
+        {
+            if (!GetComponent<AudioSource>().isPlaying)
+            {
+                
+            }
+        }
+
+
+    }
 
 }
