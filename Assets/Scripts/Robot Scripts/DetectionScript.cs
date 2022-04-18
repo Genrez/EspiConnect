b28d52ffc835a684457966d1fd10e1065888748f
EdoardoMenon <edoardomenon1997@gmail.com>
2022-04-18 13:57:18 +1000

    Fixed tutorial Scene

--- Assets/Scripts/Robot Scripts/DetectionScript.cs
+++ Assets/Scripts/Robot Scripts/DetectionScript.cs
@@ -8,18 +8,22 @@ public class DetectionScript : MonoBehaviour
     private PolygonCollider2D polygonCollider;
     private BoxCollider2D playerCollider;
     public RobotController robotcontroller;
+    private PolygonCollider2D edgeCollider1;
+    private PolygonCollider2D edgeCollider2;
 
     void Start()
     {
         playerCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
+        edgeCollider1 = GetComponent<PolygonCollider2D>();
+        edgeCollider2 = GetComponent<PolygonCollider2D>();
         polygonCollider = GetComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (capsuleCollider.IsTouching(playerCollider) || polygonCollider.IsTouching(playerCollider)) {
+        if (capsuleCollider.IsTouching(playerCollider) || polygonCollider.IsTouching(playerCollider) || edgeCollider1.IsTouching(playerCollider) || edgeCollider2.IsTouching(playerCollider)) {
             robotcontroller.IsPlayerInView(true);
         }
         else {
