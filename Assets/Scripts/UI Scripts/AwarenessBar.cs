448a049fc3a591b3229b795e854c24326bb5de57
STEVEN <zelong.cai-1@student.uts.edu.au>
2022-04-19 19:34:09 +0800

    Made some simple UI

---
+++ Assets/Scripts/UI Scripts/AwarenessBar.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AwarenessBar : MonoBehaviour
+{
+    private Awareness awareness;
+    private Image barImage;
+    public GameObject target;
+    private void Awake()
+    {
+        barImage = transform.Find("bar").GetComponent<Image>();
+
+        awareness = new Awareness();
+    }
+
+    private void Update()
+    {
+        awareness.Update();
+
+        barImage.fillAmount = awareness.GetAwarenessNormalized();
+        if(barImage.fillAmount == 1)
+        {
+            target.SetActive(true);
+        }
+    }
+}
+
+public class Awareness
+{
+    public const int Awareness_MAX = 100;
+
+    private float awarnessAmount;
+    private float awarnessRegenAmount;
+
+    public Awareness()
+    {
+        awarnessAmount = 0;
+        awarnessRegenAmount = 20f;
+    }
+
+    public void Update()
+    {
+        awarnessAmount += awarnessRegenAmount * Time.deltaTime;
+    }
+    public float GetAwarenessNormalized()
+    {
+        return awarnessAmount / Awareness_MAX;
+    }
+}
