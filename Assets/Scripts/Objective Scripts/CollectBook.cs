f04c7b20760c6f7ae2dc64f24cdfaa6ea9585938
GenRex <richardngo2000@gmail.com>
2022-05-13 02:18:37 +1000

    Update

--- Assets/Scripts/Objective Scripts/CollectBook.cs
+++ Assets/Scripts/Objective Scripts/CollectBook.cs
@@ -7,8 +7,6 @@ public class CollectBook : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Works");
-        Destroy(gameObject);
         SceneManager.LoadScene("VictoryScreen");
     }
 }
